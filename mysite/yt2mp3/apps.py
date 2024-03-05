eb60f5e25912f9b5f8cff3a93716426f7521fb3a
Nicholas Balsom <95197246+NicholasBalsom@users.noreply.github.com>
2024-03-05 11:49:19 -0330

    started django site

---
+++ mysite/yt2mp3/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Yt2Mp3Config(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "yt2mp3"
---
+++ mysite/yt2mp3/utils.py
@@ -0,0 +1,1 @@
+# Write Util here
