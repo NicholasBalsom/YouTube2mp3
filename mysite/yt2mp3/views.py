e1d598f5562f0b70e088c7aa892461e31280579f
Nicholas Balsom <95197246+NicholasBalsom@users.noreply.github.com>
2024-03-07 15:04:09 -0330

    update index.html

--- mysite/yt2mp3/views.py
+++ mysite/yt2mp3/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, "yt2mp3/index.html", {
-        
-    })
+    return render(request, "yt2mp3/index.html", {})
