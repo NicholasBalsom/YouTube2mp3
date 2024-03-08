362674a51ccca8935fdfed2c081322733f4dc25f
Nicholas Balsom <95197246+NicholasBalsom@users.noreply.github.com>
2024-03-08 10:29:07 -0330

    added spotify and youtube

--- mysite/yt2mp3/views.py
+++ mysite/yt2mp3/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "yt2mp3/index.html", {})
+
+
+def youtube(request):
+    return render(request, "yt2mp3/youtube.html")
+
+
+def spotify(request):
+    return render(request, "yt2mp3/spotify.html")
