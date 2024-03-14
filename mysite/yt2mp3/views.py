44cf2f8e5bba0d6132ee34d7ae3ae980569ae4e3
NicholasBalsom <NicholasBalsom@gmail.com>
2024-03-14 18:25:43 -0230

    comments

--- mysite/yt2mp3/views.py
+++ mysite/yt2mp3/views.py
@@ -4,14 +4,11 @@ from . import utils
 from .forms import NewSpotipyForm, NewYouTubeForm
 
 
-# Home page view
 def index(request):
     return render(request, "yt2mp3/index.html")
 
 
-# YouTube download view
 def youtube(request):
-    # If form is submitted with methood = 'post'
     if request.method == "POST":
         # Create a form with the post data
         form = NewYouTubeForm(request.POST)
@@ -24,14 +21,11 @@ def youtube(request):
             return render(request, "yt2mp3/youtube.html", {"message": "Video was unable to be downloaded."})
 
     else:
-        # Create a new blank form object
         form = NewYouTubeForm()
         return render(request, "yt2mp3/youtube.html", {"form": form})
 
 
-# Spotify download view
 def spotify(request):
-    # If form is submitted with methood = 'post'
     if request.method == "POST":
         form = NewSpotipyForm(request.POST)
         if form.is_valid():
@@ -39,6 +33,5 @@ def spotify(request):
             # fileset = utils.spotify(url)
             # return render()
     else:
-        # Create a new blank spotipyform object
         form = NewSpotipyForm()
         return render(request, "yt2mp3/spotify.html", {"form": form})
