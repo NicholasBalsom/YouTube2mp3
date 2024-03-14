ff67bf3de37d35dda665f418eb4de923e397c442
NicholasBalsom <NicholasBalsom@gmail.com>
2024-03-14 18:19:52 -0230

    added comments

--- mysite/yt2mp3/forms.py
+++ mysite/yt2mp3/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 
 
+# Django form for YouTube URLs
 class NewYouTubeForm(forms.Form):
     url = forms.URLField(label="Video URL")
 
 
+# Django form for Spotify URLs
 class NewSpotipyForm(forms.Form):
     url = forms.URLField(label="Spotify Album/Playlist URL")
--- mysite/yt2mp3/views.py
+++ mysite/yt2mp3/views.py
@@ -1,33 +1,44 @@
 from django.shortcuts import render
 
+from . import utils
 from .forms import NewSpotipyForm, NewYouTubeForm
 
 
+# Home page view
 def index(request):
     return render(request, "yt2mp3/index.html")
 
 
+# YouTube download view
 def youtube(request):
+    # If form is submitted with methood = 'post'
     if request.method == "POST":
+        # Create a form with the post data
         form = NewYouTubeForm(request.POST)
+
         if form.is_valid():
             url = form.cleaned_data["url"]
-            # file = util.youtube(url)
+            # file = utils.youtube(url)
             # return render(request, "yt2mp3/youtube.html", {"file": file})
         else:
             return render(request, "yt2mp3/youtube.html", {"message": "Video was unable to be downloaded."})
+
     else:
+        # Create a new blank form object
         form = NewYouTubeForm()
         return render(request, "yt2mp3/youtube.html", {"form": form})
 
 
+# Spotify download view
 def spotify(request):
+    # If form is submitted with methood = 'post'
     if request.method == "POST":
         form = NewSpotipyForm(request.POST)
         if form.is_valid():
             url = form.cleaned_data["url"]
-            # fileset = util.spotify(url)
+            # fileset = utils.spotify(url)
             # return render()
     else:
+        # Create a new blank spotipyform object
         form = NewSpotipyForm()
         return render(request, "yt2mp3/spotify.html", {"form": form})
