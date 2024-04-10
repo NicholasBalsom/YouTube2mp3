2b25865b341e5cf8090282f33221f3412490646e
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-04-10 13:07:24 -0230

    Just trying to make a hamburger nav thing

--- mysite/yt2mp3/static/yt2mp3/user.js
+++ mysite/yt2mp3/static/yt2mp3/user.js
@@ -2,11 +2,21 @@ function ThemeToggle() {
     const body = document.body;
     const links = document.querySelectorAll('a');
     const inputs = document.querySelectorAll('input');
+
     links.forEach((link) => {
         link.classList.toggle('light-mode');
     });
-    body.classList.toggle('light-mode');
+    
     inputs.forEach((input) => {
         input.classList.toggle('light-mode-special');
+    });
+
+    body.classList.toggle('light-mode');
+}
+
+function BurgerNav() {
+    const spans = document.querySelectorAll('span');
+    spans.forEach((span) => {
+        span.classList.toggle('is-active');
     });
 }
