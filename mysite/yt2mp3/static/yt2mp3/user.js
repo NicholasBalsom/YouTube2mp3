c99c5dbacb84412f80cf3d9d7059f53cd2c65ce5
swordsounds <126785544+swordsounds@users.noreply.github.com>
2024-05-31 11:13:21 -0230

    Why must god punish me

--- mysite/yt2mp3/static/yt2mp3/user.js
+++ mysite/yt2mp3/static/yt2mp3/user.js
@@ -1,13 +1,17 @@
 function ThemeToggle() {
     const body = document.body;
-    const links = document.querySelectorAll('a');
+    // const links = document.querySelectorAll('a');
+    const texts = document.querySelectorAll(['h1', 'p', 'label', 'a'])
     const inputs = document.querySelectorAll('input');
     const buttons = document.querySelectorAll('button');
 
-    links.forEach((link) => {
-        link.classList.toggle('light-mode');
-    });
+    // links.forEach((link) => {
+    //     link.classList.toggle('light-mode-text');
+    // });
     
+    texts.forEach((text) => {
+        text.classList.toggle('light-mode-text');
+    });
     inputs.forEach((input) => {
         input.classList.toggle('light-mode-special');
     });
