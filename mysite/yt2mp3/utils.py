faff9c44e3ca2b097ab78fe164b7fdca0228d02f
Nicholas Balsom <95197246+NicholasBalsom@users.noreply.github.com>
2024-06-14 20:57:14 -0230

    Added comments

--- mysite/yt2mp3/utils.py
+++ mysite/yt2mp3/utils.py
@@ -7,61 +7,61 @@ from dotenv import load_dotenv
 from pytube import Playlist, Search, YouTube, exceptions
 from spotipy.oauth2 import SpotifyOAuth
 
-# from tabulate import tabulate
-
 # test_url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUJcmljayByb2x'
 # test_playlist: 'https://www.youtube.com/playlist?list=PLP3kvqg_Ut86PFpWW3orO3DXiIpK-B-GI'
 # test_album:'https://open.spotify.com/album/05jbNkYoEQdjVDHEHtg1gY?si=gEMXkv9TQ_OEBMBu7whr2A'
 # test_playlist_spoty: "https://open.spotify.com/playlist/5OO6uyRtTv1w6rMiKdqHPV?si=2d1a8f8ee4e04c95"
 
+
+# Load environment variables from .env
 load_dotenv()
 SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
 SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
 
-SUCCESS = "Success"
-FAILED = "Failed"
 URL_REGEX_ERROR = "\n! Url does not match requested format. !"
 
 
-def youtube_playlist(p_url, song_list):
-    playlist = Playlist(p_url)
-    for url in playlist.video_urls:
-        upload_audio(url)
-    return song_list
-
-
+# Used for removing temprary files in tmp/ folder
 def clean_tmp():
+    # Traverse songs directory
     for folder_name, sub_folders, file_names in os.walk("yt2mp3/tmp/songs"):
+        # Delete each file in the songs directory
         for filename in file_names:
             file_path = os.path.join(folder_name, filename)
             os.remove(file_path)
 
 
 def spotify(s_url):
+    # Authorize access to the Spotify API
     auth_manager = SpotifyOAuth(scope="user-library-read")
     sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    # Confifim that the imputted url is a valid spotify URL
     if re.search(r"https:\/\/open.spotify.com\/(playlist|album|track)\/[A-Za-z0-9?=_-]{22,}(?:$|\?)", s_url):
+        # For Spotify Albums
         if "album" in s_url:
             for song in sp.album_tracks(s_url)["items"]:
-                song_name = song["name"]
-                artist_name = song["artists"][0]["name"]
+                song_name = song["name"]  # Get song name
+                artist_name = song["artists"][0]["name"]  # Get the first artists name
                 vid = search_video(song_name, artist_name)
                 upload_audio(vid)
             return upload_zip()
 
+        # For Spotify Playlists
         elif "playlist" in s_url:
             for song in sp.playlist_tracks(s_url)["items"]:
-                song_name = song["track"]["name"]
-                artist_name = song["track"]["artists"][0]["name"]
+                song_name = song["track"]["name"]  # Song name
+                artist_name = song["track"]["artists"][0]["name"]  # Get first artists name
                 vid = search_video(song_name, artist_name)
                 upload_audio(vid)
             return upload_zip()
 
+        # For  single Spotify song links
         elif "track" in s_url:
             track = sp.track(s_url)
-            song_name = track["name"]
-            artist_name = track["artists"][0]["name"]
+            song_name = track["name"]  # Song name
+            artist_name = track["artists"][0]["name"]  # First artist name
             vid = search_video(song_name, artist_name)
             return upload_audio(vid)
 
@@ -69,43 +69,48 @@ def spotify(s_url):
         print(URL_REGEX_ERROR)
 
 
+# Searches up songs on YouTube for downloading
 def search_video(song_name, artist_name):
     try:
+        # Try searching the song
         s = Search(f"{song_name} {artist_name}")
     except exceptions.PytubeError:
         raise ValueError("Pytube Search Error")
     else:
-        result = s.results[0]
-        return result.watch_url
+        result = s.results[0]  # take first result
+        return result.watch_url  # Return the url
 
 
 def upload_zip():
+    # If a zip folder doesnt exsist in tmp/ create one
     if not os.path.exists("yt2mp3/tmp/zip"):
         os.makedirs("yt2mp3/tmp/zip")
 
     with ZipFile("yt2mp3/tmp/zip/songs.zip", "w") as zip_object:
-        # Traverse all files in directory
+        # Traverse the songs directory
         for folder_name, sub_folders, file_names in os.walk("yt2mp3/tmp/songs"):
             for filename in file_names:
-                # Create filepath of files in directory
+                # Create filepath
                 file_path = os.path.join(folder_name, filename)
                 # Add files to zip file
                 zip_object.write(file_path, os.path.basename(file_path))
 
+        # Check to make sure the zip file exists
         if os.path.exists("yt2mp3/tmp/zip/songs.zip"):
             print("ZIP file created")
-            return "songs.zip"
+            return "songs.zip"  # Return zip file name
         else:
             print("ZIP file not created")
 
 
 def upload_audio(yt_url):
+    # Validate the youtube url
     if re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11}).*", yt_url):
         yt = YouTube(yt_url)
-        video_title = re.sub(r"[^\w\-_\. ]", "_", yt.title)
-        stream = yt.streams.filter(only_audio=True).first()
+        video_title = re.sub(r"[^\w\-_\. ]", "_", yt.title)  # Get title and remove and unessisary characters
+        stream = yt.streams.filter(only_audio=True).first()  # Get audio from video
         mp3_filename = f"{video_title}.mp3"
-        stream.download(filename=mp3_filename, output_path="yt2mp3/tmp/songs")
+        stream.download(filename=mp3_filename, output_path="yt2mp3/tmp/songs")  # Download to web server
         return mp3_filename
     else:
         print("Invlaid url:")
--- mysite/yt2mp3/views.py
+++ mysite/yt2mp3/views.py
@@ -8,6 +8,7 @@ from . import utils
 from .forms import NewSpotipyForm, NewYouTubeForm
 
 
+# Home page
 def index(request):
     return render(request, "yt2mp3/index.html")
 
@@ -15,18 +16,19 @@ def index(request):
 def youtube(request):
     if request.method == "POST":
         utils.clean_tmp()
+
         # Create a form with the post data
         form = NewYouTubeForm(request.POST)
 
         if form.is_valid():
-            url = form.cleaned_data["url"]
-            mp3_filename = utils.upload_audio(url)
-            print(mp3_filename)
+            url = form.cleaned_data["url"]  # get the url
+            mp3_filename = utils.upload_audio(url)  # upload the mp3 and get the filename
             return render(request, "yt2mp3/youtube.html", {"file": mp3_filename})
         else:
             return render(request, "yt2mp3/youtube.html", {"message": "Video was unable to be downloaded."})
 
     else:
+        # Create new form
         form = NewYouTubeForm()
         return render(request, "yt2mp3/youtube.html", {"form": form})
 
@@ -34,28 +36,34 @@ def youtube(request):
 def spotify(request):
     if request.method == "POST":
         utils.clean_tmp()
+        # # Create a form object with the post data
         form = NewSpotipyForm(request.POST)
         if form.is_valid():
-            url = form.cleaned_data["url"]
-            filename = utils.spotify(url)
+            url = form.cleaned_data["url"]  # get url
+            filename = utils.spotify(url)  # download the audio(s) using spotify utils
             return render(request, "yt2mp3/spotify.html", {"file": filename})
     else:
+        # Create a new form
         form = NewSpotipyForm()
         return render(request, "yt2mp3/spotify.html", {"form": form})
 
 
+# Not a view, is used to download files from your browser
 def download(request, filename: str):
     if filename.endswith(".mp3"):
+        # get path to uploaded files (web server)
         path = os.path.join(settings.MEDIA_ROOT, "yt2mp3/tmp/songs", filename)
         content_type = "audio/mpeg"
     else:
+        # get path to uploaded files (web server)
         path = os.path.join(settings.MEDIA_ROOT, "yt2mp3/tmp/zip", filename)
         content_type = "application/zip"
 
     if os.path.exists(path):
         with open(path, "rb") as file:
+            # Create a response to download the file
             response = HttpResponse(file.read(), content_type=content_type)
-            response["Content-Disposition"] = 'attachment; filename="{}"'.format(filename)
+            response["Content-Disposition"] = f"attachment; filename='{filename}'"
             return response
     else:
         return HttpResponse("File not found", status=404)
