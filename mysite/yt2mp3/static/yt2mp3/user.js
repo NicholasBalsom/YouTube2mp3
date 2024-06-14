c9f2b17a15c56f920c0812795d373d382bf07aca
Nicholas Balsom <95197246+NicholasBalsom@users.noreply.github.com>
2024-06-14 08:46:47 -0230

    im working on it

---
+++ mysite/yt2mp3/static/yt2mp3/user.js
@@ -0,0 +1,24 @@
+function ThemeToggle() {
+    const body = document.body;
+    // const links = document.querySelectorAll('a');
+    const texts = document.querySelectorAll(['h1', 'p', 'a'])
+    const inputs = document.querySelectorAll(['input', 'label']);
+    const buttons = document.querySelectorAll('button');
+
+    // links.forEach((link) => {
+    //     link.classList.toggle('light-mode-text');
+    // });
+    
+    texts.forEach((text) => {
+        text.classList.toggle('light-mode-text');
+    });
+    inputs.forEach((input) => {
+        input.classList.toggle('light-mode-special');
+    });
+
+    buttons.forEach((button) => {
+        button.classList.toggle('light-mode-button');
+    });
+
+    body.classList.toggle('light-mode');
+}
